ee039fb5ff67e8715038f209f29bb489b33ac0f3
Ubuntu <lianapatel515@gmail.com>
2024-06-11 18:16:53 +0000

    small files

---
+++ run_simple_test.sh
@@ -0,0 +1,44 @@
+export debugSearchFlag=0
+#! /bin/bash
+
+
+
+cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -B build
+
+make -C build -j faiss
+make -C build utils
+make -C build test_acorn
+
+
+
+##########################################
+# TESTING SIFT1M and PAPER
+##########################################
+now=$(date +"%m-%d-%Y")
+
+
+# run of sift1M test
+N=1000000 
+gamma=12 
+dataset=sift1M_test 
+M=32 
+M_beta=64
+
+
+
+parent_dir=${now}_${dataset}
+mkdir ${parent_dir}
+dir=${parent_dir}/MB${M_beta}
+mkdir ${dir}
+
+TZ='America/Los_Angeles' date +"Start time: %H:%M" &>> ${dir}/summary_sift_n=${N}_gamma=${gamma}.txt
+
+
+./build/demos/test_acorn $N $gamma $dataset $M $M_beta  &>> ${dir}/summary_sift_n=${N}_gamma=${gamma}.txt
+
+     
+
+
+
+
+
