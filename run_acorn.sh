89de115421383c6ea598c20f7c50046be33cc350
Haiyang <3389245+HaiyangXu@users.noreply.github.com>
2024-09-26 14:20:49 -0700

    fix scripts

--- run_acorn.sh
+++ run_acorn.sh
@@ -7,7 +7,7 @@ cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=ON -DBUIL
 
 #make -C build -j faiss
 #make -C build utils
-make -C build acorn
+make -C build -j acorn
 
 
 #../DiskANN/build/apps/utils/compute_filtered_groundtruth  --K 10 --base_file ./base.fbin --base_labels ./base_labels.txt --dist_fn l2 --data_type float --gt_file gt.bin --query_file query.fbin --query_label ./query_labels.txt 
@@ -18,18 +18,24 @@ make -C build acorn
 now=$(date +"%m-%d-%Y")
 
 
- 
-
-
+ # run  
+N=1000000
+nq=10 
+gamma=12 
+dataset=amazon
+M=32 
+M_beta=64
+$gt = "gt_1m.bin.txt"
+$efs = 0 # 0 for build, >1 for search only
 parent_dir=${now}_${dataset}
 mkdir ${parent_dir}
 dir=${parent_dir}/MB${M_beta}
 mkdir ${dir}
 
-TZ='America/Los_Angeles' date +"Start time: %H:%M" &>> ${dir}/summary_sift_n=${N}_gamma=${gamma}.txt
+TZ='America/Los_Angeles' date +"Start time: %H:%M" &>> ${dir}/summary_${dataset}_n=${N}_gamma=${gamma}.txt
 
 
-./build/demos/acorn $N 100 ../amazon/    &>> ${dir}/summary_sift_n=${N}_gamma=${gamma}.txt
+./build/demos/acorn $N $nq ../$dataset/ . . $gt 100 $M_beta $efs  &>> ${dir}/summary_${dataset}_n=${N}_gamma=${gamma}.txt
 
 
 
